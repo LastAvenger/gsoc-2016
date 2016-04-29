d6e5e9bcb57ba3b03aed1f987562ad56bb265010
lastavengers <lastavengers@outlook.com>
2016-04-29 21:59:32 +0800

    mount hd.img as home, used to store code

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 [[ "$1" != "-g" ]] && arg="-display none"
 
-qemu-system-i386 -enable-kvm  -m 1G                         \
-    -drive cache=writeback,file=debian-hurd.img,format=raw  \
-    -net nic -net user,hostfwd=tcp::5555-:22                \
+qemu-system-i386 -enable-kvm  -m 1G                                 \
+    -drive index=0,cache=writeback,file=debian-hurd.img,format=raw  \
+    -drive index=1,cache=directsync,file=hd.img,format=raw           \
+    -net nic -net user,hostfwd=tcp::5555-:22                        \
     $arg & sleep 3 && ssh -p 5555 la@localhost

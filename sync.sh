cf5204e70fca0d45e0bc3b3d995944fd3a50b4ef
lastavengers <lastavengers@outlook.com>
2016-05-23 20:33:26 +0800

    image sync script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/sh
+# ./sync -c|-i [g2h h2g]
+# -c: rsync code from guest to host ($GUEST_DEST -> $HOST_DEST)
+# -i: rsync a ext2 image between guest and host ($GUEST_DEST <-> $HOST_DEST)
 
-rsync -avzP hurd la@localhost:.
+GUSET_DEST="\."
+HOST_DEST="/home/la/git/gsoc-2016"
+
+if [[ "$1" == "-c" || -z "$1" ]]; then
+    rsync -avzP $HOST_DEST/hurd la@localhost:$GUEST_DEST
+fi
+
+if [[ "$1" == "-i" ]]; then
+    if [[ "$2" == "g2h" ]]; then
+        rsync -avzP la@localhost:"\./ext2.img" $HOST_DEST
+    elif [[ "$2" == "h2g" ]]; then
+        rsync -avzP $HOST_DEST/ext2.img la@localhost:$GUSET_DEST
+    fi
+fi
